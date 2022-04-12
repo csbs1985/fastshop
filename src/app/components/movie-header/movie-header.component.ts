29e2d212774dea130f7b04cac044046be207733e
Charles Santos <csbs.conta@outlook.com>
2022-04-12 13:30:43 -0300

    up banner in movie-header

--- src/app/components/movie-header/movie-header.component.ts
+++ src/app/components/movie-header/movie-header.component.ts
@@ -9,7 +9,10 @@ import { DatePipe } from '@angular/common';
 export class MovieHeaderComponent implements OnInit {
   @Input() content!: any;
 
-  readonly URL_IMAGES: string =
+  readonly URL_BANNER: string =
+    'https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces';
+
+  readonly URL_POSTER: string =
     'https://www.themoviedb.org/t/p/w220_and_h330_face';
 
   public movie: any;
@@ -22,10 +25,10 @@ export class MovieHeaderComponent implements OnInit {
 
   private fillMovieHeader() {
     this.movie = {
-      backdrop_path: this.URL_IMAGES + this.content.backdrop_path,
+      backdrop_path: this.URL_BANNER + this.content.backdrop_path,
       genres: this.content.genres,
       overview: this.content.overview,
-      poster_path: this.URL_IMAGES + this.content.poster_path,
+      poster_path: this.URL_POSTER + this.content.poster_path,
       release_date: this.datePipe.transform(
         this.content.release_date,
         'dd/MM/yyyy'
