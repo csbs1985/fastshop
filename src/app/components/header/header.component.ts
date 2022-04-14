a3e432dae7bdc35358d6990de7d43c2204c39559
Charles Santos <csbs.conta@outlook.com>
2022-04-14 09:03:08 -0300

    header responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public image =
+    'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg';
+
   public menu: string[];
   public submenu: string[];
 
